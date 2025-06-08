17af28c204aeb9f736a01d4ee9fec28b2a14700d
EC2 Default User <ec2-user@ip-172-31-83-67.ec2.internal>
2025-06-08 08:37:29 +0000

    Added HelloWorld.java and pom.xml

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+cat <<EOF > src/main/java/HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins + Maven!");
+    }
+}
+EOF
+
